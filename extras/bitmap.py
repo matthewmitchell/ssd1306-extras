b68490d67216b6498a78efd4a48dad40375650e2
Matt Mitchell <mattmitchell83@gmail.com>
2016-02-06 17:52:15 -0600

    Initial commit

---
+++ extras/bitmap.py
@@ -0,0 +1,124 @@
+from bitarray import bitarray
+import gaugette.ssd1306
+
+# x = col = width
+# y = row = height
+
+
+class Bitmap(object):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.bits = bitarray(width * height)
+        self.bits.setall(False)
+
+    def draw_image(self, image, x=0, y=0):
+        """Adds a PIL Image file to the bitmap
+
+        Args:
+            image (Image): Mode 1 PIL Image
+            x (int): Horizontal start position for image
+            y (int): Vertical start position for image
+        """
+        image_width, image_height = image.size
+        image_bit_array = bitarray([byte for byte in image.getdata()])
+
+        # Calculate the number of columns that fit on the bitmap
+        draw_cols = image_width if image_width + x < self.width else (self.width - x)
+
+        # Calculate the number of rows that fit on the bitmap
+        draw_rows = image_height if image_height + y < self.height else (self.height - y)
+
+        image_col_start = 0
+        # Adjust image start position and number of columns if negative horizontal position
+        if x < 0:
+            image_col_start = abs(x)
+            draw_cols = image_width - image_col_start
+            x = 0
+
+        image_row_start = 0
+        # Adjust image starting row for negative vertical position
+        if y < 0:
+            image_row_start = abs(y)
+
+        start_row = (y * self.width) + x
+
+        for i in range(image_row_start, draw_rows):
+            image_start = (i * image_width) + image_col_start
+            image_end = image_start + draw_cols
+
+            start = (i * self.width) + start_row
+            end = start + draw_cols
+
+            self.bits[start:end] = image_bit_array[image_start:image_end]
+
+    def invert(self):
+        """Inverts the bitmap
+        """
+        self.bits.invert()
+
+    def clear_block(self, x, y, col_count, row_count):
+        """Clears a block from the bitmap
+
+        Args:
+            x (int): Starting column to clear
+            y (int): Starting row to clear
+            col_count (int): Number of columns to clear
+            row_count (int): Number of rows to be cleared
+        """
+        if x >= self.width or y >= self.height:
+            return
+
+        if x < 0:
+            x = 0
+
+        if y < 0:
+            y = 0
+
+        if col_count + x > self.width:
+            col_count = self.width - x
+
+        if row_count + y > self.height:
+            row_count = self.height - y
+
+        for row in range(y, y + row_count):
+            start = (row * self.width) + y
+            end = start + col_count
+            self.bits[start:end] = False
+
+    def clear(self):
+        """Clears the bitmap
+        """
+        self.bits.setall(False)
+
+    def dump(self):
+        """Outputs a visual representation for the bitmap
+        """
+        for row in range(self.height):
+            line = ''
+            for col in range(self.width):
+                bit = self.bits[(row*self.width)+col]
+                line += '*' if bit else ' '
+            print '|{0}|'.format(line)
+
+    def get_bitmap(self):
+        """Creates a gaugette Bitmap
+        """
+        gaug_bitmap = gaugette.ssd1306.SSD1306.Bitmap(self.width, self.height)
+        page_length = self.width * 8
+        total_pages = (self.width * self.height) / page_length
+
+        gaug_bytes = []
+
+        for col in range(self.width):
+            for page in range(total_pages):
+                page_start = page * page_length
+                start = page_start + col
+                byte = 0
+                for bit in reversed(self.bits[start:page_start+page_length:self.width]):
+                    byte = (byte << 1) | bit
+                gaug_bytes.append(byte)
+
+        gaug_bitmap.data = gaug_bytes
+        return gaug_bitmap
---
+++ tests/test_bitmap.py
@@ -0,0 +1,155 @@
+import pytest
+from extras import bitmap
+from PIL import Image
+
+
+@pytest.fixture
+def small_rec():
+    """Creates a rectangle with a border
+
+    Dump of image::
+    |*****|
+    |*   *|
+    |*****|
+    """
+    width, height = (5, 3)
+    image = Image.new('1', (width, height))
+
+    for x in range(width):
+        image.putpixel((x, 0), 255)
+        image.putpixel((x, height-1), 255)
+
+    for y in range(height):
+        image.putpixel((0, y), 255)
+        image.putpixel((width-1, y), 255)
+
+    return image
+
+
+@pytest.fixture
+def bmap():
+    return bitmap.Bitmap(8, 8)
+
+
+def assert_bits_set(bmap, expected):
+    assert len(bmap.bits) == bmap.width * bmap.height, 'Unexpect bitarray length'
+    set_bits = [i for i, bit in enumerate(bmap.bits) if bit]
+
+    try:
+        assert set_bits == expected
+    except AssertionError as e:
+        message = get_visual_output(bmap, expected)
+        raise(AssertionError('{0} {1}'.format(message, e)))
+
+
+def get_visual_output(bmap, expected):
+    """Renders a visual representation of expected, set and missing bits
+    """
+    visual_output = ['\nO = Unexpected bit\nX = Missing bit\n* = Correct bit\n']
+    row_info = '|'
+    for i in range(len(bmap.bits)):
+        value = ' '
+        if bmap.bits[i]:
+            value = 'O' if i not in expected else '*'
+        elif not bmap.bits[i] and i in expected:
+            value = 'X'
+
+        row_info += value
+        if not (i+1) % bmap.width:
+            visual_output.append(row_info + '|')
+            row_info = '|'
+    return '\n'.join(visual_output) + '\n'
+
+
+class TestDrawImage(object):
+
+    def test_should_contain_image_without_offset(self, bmap, small_rec):
+        bmap.draw_image(small_rec)
+        assert_bits_set(bmap, expected=[0, 1, 2, 3, 4, 8, 12, 16, 17, 18, 19, 20])
+
+    def test_should_contain_image_with_offset(self, bmap, small_rec):
+        bmap.draw_image(small_rec, 2, 3)
+        assert_bits_set(bmap, expected=[26, 27, 28, 29, 30, 34, 38, 42, 43, 44, 45, 46])
+
+    def test_should_contain_image_with_horizontal_overflow(self, bmap, small_rec):
+        bmap.draw_image(small_rec, 5, 0)
+        assert_bits_set(bmap, expected=[5, 6, 7, 13, 21, 22, 23])
+
+    def test_should_contain_image_with_vertical_overflow(self, bmap, small_rec):
+        bmap.draw_image(small_rec, 0, 6)
+        assert_bits_set(bmap, expected=[48, 49, 50, 51, 52, 56, 60])
+
+    def test_should_contain_image_with_negative_margins(self, bmap, small_rec):
+        bmap.draw_image(small_rec, -2, -1)
+        assert_bits_set(bmap, expected=[2, 8, 9, 10])
+
+    def test_should_contain_multiple_images(self, bmap, small_rec):
+        expected = [0, 1, 2, 3, 4, 8, 12, 16, 17, 18, 19, 20, 24, 25, 26, 27, 28, 32, 36, 40, 41, 42, 43, 44]
+        bmap.draw_image(small_rec, 0, 0)
+        bmap.draw_image(small_rec, 0, 3)
+        assert_bits_set(bmap, expected=expected)
+
+
+class TestGetBitmap(object):
+
+    def test_should_return_valid_bitmap(self, small_rec):
+        expected = [7, 0, 5, 0, 5, 0, 5, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 5, 0, 5, 0,
+                    229, 0, 167, 0, 160, 0, 160, 0, 224]
+        bmap = bitmap.Bitmap(16, 16)
+        bmap.draw_image(small_rec, 0, 0)
+        bmap.draw_image(small_rec, 8, 8)
+        bmap.draw_image(small_rec, 11, 13)
+        gaugette_bitmap = bmap.get_bitmap()
+
+        assert gaugette_bitmap.data == expected
+
+
+class TestInvert(object):
+
+    def test_should_invert_empty_bitmap(self, bmap):
+        expected = [i for i in range(bmap.width * bmap.height)]
+        bmap.invert()
+        assert_bits_set(bmap, expected)
+
+    def test_should_invert_bitmap_with_image(self, bmap, small_rec):
+        expected = [5, 6, 7, 9, 10, 11, 13, 14, 15, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+                    36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58,
+                    59, 60, 61, 62, 63]
+
+        bmap.draw_image(small_rec, 0, 0)
+        bmap.invert()
+        assert_bits_set(bmap, expected=expected)
+
+
+class TestClear(object):
+
+    def test_should_clear_empty_bitmap(self, bmap):
+        bmap.clear()
+        assert_bits_set(bmap, expected=[])
+
+    def test_should_clear_bitmap_with_image(self, bmap, small_rec):
+        bmap.draw_image(small_rec, 0, 0)
+        bmap.clear()
+        assert_bits_set(bmap, expected=[])
+
+
+class TestClearBlock(object):
+
+    def test_should_clear_block_from_empty_bitmap(self, bmap):
+        bmap.clear_block(0, 0, 2, 2)
+        assert_bits_set(bmap, expected=[])
+
+    def test_should_clear_part_of_image(self, bmap, small_rec):
+        bmap.draw_image(small_rec, 0, 0)
+        bmap.clear_block(0, 0, 4, 2)
+        assert_bits_set(bmap, expected=[4, 12, 16, 17, 18, 19, 20])
+
+    def test_should_clear_full_image(self, bmap):
+        bmap.invert()
+        bmap.clear_block(0, 0, 8, 8)
+        assert_bits_set(bmap, expected=[])
+
+    def test_should_clear_only_bits_within_image(self, bmap, small_rec):
+        bmap.draw_image(small_rec, 0, 0)
+        bmap.clear_block(1, 1, 1000, 1000)
+        assert_bits_set(bmap, expected=[0, 1, 2, 3, 4, 8, 16])
